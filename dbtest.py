44070a4539990cc1b0528ba9401c9aeafbab2a46
Lathish <44872859+lathishchowdary55@users.noreply.github.com>
2020-07-10 14:13:07 +0530

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,31 +1,38 @@
-
 import sqlite3
 #from guide.api.settings import DATABASES
-import DATABASES
+#import DATABASES
 import pyodbc
 import mysql.connector
 import  psycopg2
+import json
 Database =input("Databasename ")
-if Database =="SQLserver":
+d= Database.upper()
+if Database=='sqlserver':
     user_input =True
    # SQL server connections
     while user_input:
-        conn= pyodbc.connect(host=input("host "),
-        user = input("user name "),
-        password=input("password "),
-        Driver='ODBC Driver 13 for SQL Server',
-        database=input("database "))
-        if True:
-            print("connection is successful")
-            break
-        else:
-            print("not connected")
+        try:
+            conn= pyodbc.connect(host=input("host "),
+            user = input("user name "),
+            password=input("password "),
+            Driver='ODBC Driver 13 for SQL Server')
+            #database=input("database "))
+            if True:
+                print("connection is successful")
+                break
+        except Exception:
+            print(" connection failed please check the username and password")
 
     mycursor =conn.cursor()
-    mycursor.execute("select Table_Name from INFORMATION_SCHEMA.TABLES")
+    mycursor.execute("""select  
+                            TABLE_CATALOG as [Database],
+                            [TABLE_SCHEMA],TABLE_NAME,
+                            count(COLUMN_NAME)as column_count from INFORMATION_SCHEMA.COLUMNS
+                            Group by TABLE_CATALOG,[TABLE_SCHEMA],TABLE_NAME 
+                            order by TABLE_NAME""")
 
     for i in mycursor:
-        print(i)
+        print(json.loads(i))
 
     Table = input("you want to select Table-y/n:")
     if Table=="y":
@@ -36,8 +43,10 @@ if Database =="SQLserver":
                 print(i)
         except Exception:
             print("selected table not present in a database")
-    else:
+    elif Table=="n":
         print("Thank you")
+    else:
+        print("Wrong input please select y/n")
 
 #--------------------------------------- mysql connections------------------------------------------------
 elif Database =="Mysql":
@@ -46,8 +55,8 @@ elif Database =="Mysql":
     while user_input:
         conn= mysql.connector.connect(host=input("host "),
         user = input("user name "),
-        password=input("password "),
-        database=input("database "))
+        password=input("password "))
+        #database=input("database "))
         if True:
             print("connection is successful")
             break
@@ -84,7 +93,7 @@ elif  Database =="Postgresql":
             print("not connected")
 
     mycursor =conn.cursor()
-    mycursor.execute("Show tables")
+    mycursor.execute("Show databases")
 
     for i in mycursor:
         print(i)
@@ -118,7 +127,7 @@ elif  Database =="SQLlite":
     Table = input("you want to select Table-y/n:")
     if Table=="y":
         try:
-            Tablename=input("Table name ")
+            Tablename=input(" nameTable ")
             mycursor.execute(f"select * from {Tablename}")
             for i in mycursor:
                 print(i)
