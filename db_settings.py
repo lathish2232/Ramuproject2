67903d16b05019612a79bdc4fd6beeb9381c5339
Lathish <44872859+lathishchowdary55@users.noreply.github.com>
2020-07-10 14:15:57 +0530

    Add files via upload

---
+++ db_settings.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR =os.path.dirname(os.path.abspath(__file__))
+DATABASES ={
+    'sqllite':os.path.join(BASE_DIR, 'Guid.db')
+}
+                                            
+
+
+
